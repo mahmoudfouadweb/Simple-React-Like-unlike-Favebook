a6e3e1813796107e00b83f58e736af087094c016
mahmoud fouad <mfapolice@gmail.com>
2022-08-20 08:54:48 +0200

    add route and switch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetupsPage';
 import FavoritesPage from './pages/FavoritesPage';
 import NewMeetupPage from './pages/NewMeetupPage';
@@ -5,12 +6,22 @@ import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
-    <>
-      <AllMeetupsPage />
-      <NewMeetupPage />
-      <FavoritesPage />
-      <ProfilePage />
-    </>
+    <div>
+      <Switch>
+        <Route path={'/'} exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path={'/new-meetup'}>
+          <NewMeetupPage />
+        </Route>
+        <Route path={'/my-favorites'}>
+          <FavoritesPage />
+        </Route>
+        <Route path={'/my-profile'}>
+          <ProfilePage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
