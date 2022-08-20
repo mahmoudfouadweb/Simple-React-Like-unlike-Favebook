37143038d22f1688e3bfff6905209aa3b7abb9e1
mahmoud fouad <mfapolice@gmail.com>
2022-08-21 00:14:43 +0200

    add addMeetupHandler function to post request

--- src/components/meetups/MeetupForm.js
+++ src/components/meetups/MeetupForm.js
@@ -1,9 +1,61 @@
+import { useRef } from 'react';
+import Card from '../../ui/Card';
+
+import classes from './MeetupForm.module.css';
+
 function MeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    console.log(Object.keys(meetupData).length);
+  }
   return (
-    <form>
-      <label htmlFor="">Title</label>
-      <input type="text" id="" />
-    </form>
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" id="title" required ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" id="image" required ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address"> Address</label>
+          <input type="text" id="address" required ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description"> Description</label>
+          <textarea
+            type="text"
+            id="description"
+            rows={3}
+            required
+            ref={descriptionInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>add meetup</button>
+        </div>
+      </form>
+    </Card>
   );
 }
 
--- src/pages/NewMeetupPage.js
+++ src/pages/NewMeetupPage.js
@@ -1,7 +1,11 @@
+import MeetupForm from '../components/meetups/MeetupForm';
+
 function NewMeetupPage() {
+  function addMeetupHandler(meetupObj) {}
   return (
     <section>
       <h1>New Meetup</h1>
+      <MeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
